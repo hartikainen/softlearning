f354e0e794cabd87b3355af099da171258c7c918
Kristian Hartikainen <hartikainen@berkeley.edu>
2019-01-11 15:19:02 -0800

    Resurrect rllab environments

--- softlearning/environments/utils.py
+++ softlearning/environments/utils.py
@@ -2,12 +2,20 @@ from .adapters.gym_adapter import (
     GYM_ENVIRONMENTS,
     GymAdapter,
 )
+from .adapters.rllab_adapter import (
+    RLLAB_ENVIRONMENTS,
+    RllabAdapter,
+)
+
+
 ENVIRONMENTS = {
     'gym': GYM_ENVIRONMENTS,
+    'rllab': RLLAB_ENVIRONMENTS
 }
 
 ADAPTERS = {
     'gym': GymAdapter,
+    'rllab': RllabAdapter,
 }
 
 
