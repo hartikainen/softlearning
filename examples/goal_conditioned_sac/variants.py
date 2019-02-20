a012fea3081ac4e19dc9460f43ac60dacee25f87
Kristian Hartikainen <hartikainen@berkeley.edu>
2019-02-19 19:14:41 -0800

    Update variants for goal conditioned sac

--- examples/goal_conditioned_sac/variants.py
+++ examples/goal_conditioned_sac/variants.py
@@ -13,19 +13,22 @@ def get_variant_spec(args):
     variant_spec['Q_params']['type'] = (
         'double_goal_conditioned_feedforward_Q_function')
     variant_spec['algorithm_params']['type'] = 'HERSAC'
-    variant_spec['algorithm_params']['kwargs'].update({
-        'eval_n_episodes': 20,
-        'plot_distances': False,
-    })
+    variant_spec['algorithm_params']['kwargs'] = {
+        key: value
+        for key, value in
+        metric_learning_variant_spec['algorithm_params']['kwargs'].items()
+        if key not in ('save_full_state', 'use_distance_for')
+    }
     variant_spec['exploration_policy_params']['type'] = (
         'GoalConditionedUniformPolicy')
     variant_spec['policy_params']['type'] = 'GoalConditionedGaussianPolicy'
     variant_spec['sampler_params']['type'] = 'GoalSampler'
 
-    variant_spec['env_params'] = metric_learning_variant_spec['env_params']
+    variant_spec['env_params'] = (
+        metric_learning_variant_spec['env_params'].copy())
 
     variant_spec['replay_pool_params'] = metric_learning_variant_spec[
-        'replay_pool_params']
+        'replay_pool_params'].copy()
     variant_spec['replay_pool_params']['kwargs'].update({
         'on_policy_window': None,
         'use_distances': False,
