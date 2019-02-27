a0a518025d6ceafdcb462a26b86214ec80a02d47
Kristian Hartikainen <hartikainen@berkeley.edu>
2019-02-26 21:19:55 -0800

    Increase DClaw3 number of epochs

--- examples/development/variants.py
+++ examples/development/variants.py
@@ -112,7 +112,7 @@ NUM_EPOCHS_PER_DOMAIN = {
     'HandReach': int(1e4),
     'Point2DEnv': int(200),
     'Reacher': int(200),
-    'DClaw3': int(100),
+    'DClaw3': int(300),
     'ImageDClaw3': int(300),
     'HardwareDClaw3': int(100),
     'Pendulum': 10,
