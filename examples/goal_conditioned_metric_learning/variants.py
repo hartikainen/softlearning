d5f92b8f0e2e8439da246d7e7865375c9bbd2120
Kristian Hartikainen <hartikainen@berkeley.edu>
2019-06-01 21:14:05 -0700

    Configure HER for goal conditioned metric learning

--- examples/goal_conditioned_metric_learning/variants.py
+++ examples/goal_conditioned_metric_learning/variants.py
@@ -1,7 +1,46 @@
 from ray import tune
+import numpy as np
+
+from flatten_dict import flatten, unflatten
 
 from examples.metric_learning import (
     get_variant_spec as get_metric_learning_variant_spec)
+
+
+def terminal(batch, where_resampled, environment):
+    resampled_terminals = np.linalg.norm(
+        batch['observations']['state_desired_goal'][where_resampled]
+        - batch['goals']['state_desired_goal'][where_resampled],
+        ord=2,
+        axis=1,
+        keepdims=True,
+    ) < 0.1
+
+    batch['terminals'][where_resampled] = resampled_terminals
+
+
+def reward(batch, where_resampled, environment):
+    resampled_actions = batch['actions'][where_resampled]
+    resampled_observations = type(batch['observations'])(
+        (key, values[where_resampled])
+        for key, values in batch['observations'].items()
+    )
+
+    batch['rewards'][where_resampled] = environment.unwrapped.compute_rewards(
+        resampled_actions, resampled_observations)
+
+
+def update_batch(original_batch, resampled_batch, where_resampled):
+    batch_flat = flatten(original_batch)
+    resampled_batch_flat = flatten(resampled_batch)
+
+    batch_flat[
+        ('goals', 'state_desired_goal')
+    ][where_resampled] = resampled_batch_flat[
+        ('observations', 'state_observation')]
+
+    updated_batch = unflatten(batch_flat)
+    return updated_batch
 
 
 def get_variant_spec(args):
@@ -12,7 +51,7 @@ def get_variant_spec(args):
         'GoalConditionedMetricLearningAlgorithm')
     variant_spec['algorithm_params']['kwargs'].update({
         'eval_n_episodes': 1,
-        'plot_distances': False,
+        'plot_distances': True,
     })
     variant_spec['exploration_policy_params']['type'] = (
         'GoalConditionedUniformPolicy')
@@ -28,6 +67,9 @@ def get_variant_spec(args):
                 'type': 'future',
                 'resampling_probability': 0.8,
             },
+            'terminal_fn': tune.function(terminal),
+            'reward_fn': tune.function(reward),
+            'update_batch_fn': tune.function(update_batch),
         }
     }
 
