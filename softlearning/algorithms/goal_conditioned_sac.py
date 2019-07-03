e314c317fbe6e5b2f6b452d746e09dfb15ff5174
Kristian Hartikainen <hartikainen@berkeley.edu>
2019-07-02 22:31:35 -0700

    Fix goal conditioned sac

--- softlearning/algorithms/goal_conditioned_sac.py
+++ softlearning/algorithms/goal_conditioned_sac.py
@@ -8,6 +8,7 @@ from gym.envs.mujoco.half_cheetah_v3 import HalfCheetahEnv
 from gym.envs.mujoco.hopper_v3 import HopperEnv
 from gym.envs.mujoco.walker2d_v3 import Walker2dEnv
 
+from softlearning.models.utils import flatten_input_structure
 from softlearning.environments.utils import is_point_2d_env
 from .sac import SAC, td_target
 
@@ -22,22 +23,50 @@ class GoalConditionedSAC(SAC):
         self._target_proposer = target_proposer
         self._plot_distances = plot_distances
         self._final_exploration_proportion = final_exploration_proportion
+        self._current_goal = None
 
         super(GoalConditionedSAC, self).__init__(*args, **kwargs)
 
     def _init_placeholders(self):
         super(GoalConditionedSAC, self)._init_placeholders()
-        self._goals_ph = tf.placeholder(
-            tf.float32,
-            shape=(None, *self._observation_shape),
-            name='goals',
-        )
+        self._placeholders['goals'] = {
+            name: tf.compat.v1.placeholder(
+                dtype=(
+                    np.float32
+                    if np.issubdtype(observation_space.dtype, np.floating)
+                    else observation_space.dtype
+                ),
+                shape=(None, *observation_space.shape),
+                name=f'goals/{name}')
+            for name, observation_space
+            in self._training_environment.observation_space.spaces.items()
+        }
 
-    def _policy_inputs(self, observations):
-        return [observations, self._goals_ph]
+    def _policy_inputs(self, observations, goals=None):
+        goals = goals or self._placeholders['goals']
+        observations = {
+            name: observations[name]
+            for name in self._policy.observation_keys
+        }
+        goals = {
+            name: goals[name]
+            for name in self._policy.goal_keys
+        }
+        policy_inputs = flatten_input_structure((observations, goals))
+        return policy_inputs
 
-    def _Q_inputs(self, observations, actions):
-        return [observations, actions, self._goals_ph]
+    def _Q_inputs(self, observations, actions, goals=None):
+        goals = goals or self._placeholders['goals']
+        observations = {
+            name: observations[name]
+            for name in self._policy.observation_keys
+        }
+        goals = {
+            name: goals[name]
+            for name in self._policy.goal_keys
+        }
+        Q_inputs = flatten_input_structure((observations, actions, goals))
+        return Q_inputs
 
     def _update_goal(self):
         new_goal = self._target_proposer.propose_target(epoch=self._epoch)
@@ -93,13 +122,23 @@ class GoalConditionedSAC(SAC):
         feed_dict = super(GoalConditionedSAC, self)._get_feed_dict(
             iteration, batch)
 
-        feed_dict[self._goals_ph] = batch['goals']
+        # if self._current_goal is None:
+        #     first_observations = self._pool.last_n_batch(1)['observations']
+        #     self._current_distance_goal = type(first_observations)((
+        #         (key, values[0]) for key, values in first_observations.items()
+        #     ))
 
+        # for name, value in self._current_goal.items():
+        #     feed_dict[self._placeholders['goals'][name]] = (
+        #         value[None, ...])
+
+        # feed_dict[self._placeholders['goals']] = batch['goals']
         return feed_dict
 
     def _policy_diagnostics(self, iteration, batch):
-        policy_diagnostics = self._policy.get_diagnostics([
-            batch['observations'], batch['goals']])
+        policy_inputs = self._policy_inputs(
+            observations=batch['observations'], goals=batch['goals'])
+        policy_diagnostics = self._policy.get_diagnostics(policy_inputs)
         return policy_diagnostics
 
     def diagnostics_distances_fn(self, observations, goals):
@@ -109,14 +148,16 @@ class GoalConditionedSAC(SAC):
     def diagnostics_Q_values_fn(self, observations, goals, actions):
         # TODO(hartikainen): in point 2d plotter, make sure that
         # the observations and goals work correctly.
-        inputs = [observations, actions, goals]
-        Qs = tuple(Q.predict(inputs) for Q in self._Qs)
+        Q_inputs = self._Q_inputs(
+            observations=observations, actions=actions, goals=goals)
+        Qs = tuple(Q.predict(Q_inputs) for Q in self._Qs)
         Qs = np.min(Qs, axis=0)
         return Qs
 
     def diagnostics_V_values_fn(self, observations, goals):
         with self._policy.set_deterministic(True):
-            actions = self._policy.actions_np([observations, goals])
+            policy_inputs = self._policy_inputs(observations, goals)
+            actions = self._policy.actions_np(policy_inputs)
         V_values = self.diagnostics_Q_values_fn(observations, goals, actions)
         return V_values
 
@@ -165,28 +206,23 @@ class HERSAC(GoalConditionedSAC):
 
     def _get_Q_target(self):
         action_inputs = self._policy_inputs(
-            observations=self._next_observations_ph)
+            observations=self._placeholders['next_observations'])
         next_actions = self._policy.actions(action_inputs)
 
-        Q_inputs = self._Q_inputs(
-            observations=self._next_observations_ph, actions=next_actions)
-        next_Qs_values = tuple(Q(Q_inputs) for Q in self._Q_targets)
+        next_Q_inputs = self._Q_inputs(
+            observations=self._placeholders['next_observations'],
+            actions=next_actions)
+        next_Qs_values = tuple(Q(next_Q_inputs) for Q in self._Q_targets)
 
         min_next_Q = tf.reduce_min(next_Qs_values, axis=0)
-        next_value = min_next_Q
+        next_values = min_next_Q
 
         rewards = -1.0
-        terminals = (
-            self._terminals_ph
-            if self._ground_truth_terminals
-            else 0.0 # tf.cast(next_value > -0.5, tf.float32)
-        )
-
-        values = (1 - terminals) * next_value
+        terminals = tf.cast(self._placeholders['terminals'], next_values.dtype)
 
         Q_target = td_target(
             reward=rewards,
             discount=self._discount,
-            next_value=values)
+            next_value=(1 - terminals) * next_values)
 
         return Q_target
