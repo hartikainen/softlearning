adde7541686b4b3e0ada49fbba434a67e058b67a
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2019-10-02 17:03:21 +0300

    Update simulate-environments.sh to allow tuples

--- scripts/simulate-environments.sh
+++ scripts/simulate-environments.sh
@@ -4,7 +4,7 @@ declare -r SCRIPT_DIRECTORY="$(dirname $(realpath ${BASH_SOURCE[0]}))"
 declare -r PROJECT_ROOT="$(dirname ${SCRIPT_DIRECTORY})"
 
 RESULTS_BASE="${HOME}/ray_results/gs"
-EXPERIMENT_PATHS=(
+EXPERIMENT_PATHS_AND_CHECKPOINT_IDS=(
     # "${RESULTS_BASE}/gym/Hopper/MaxVelocity-v3/2019-07-27T20-44-40-perturbations-speed-limit-2"
     # "${RESULTS_BASE}/gym/Hopper/MaxVelocity-v3/2019-07-27T20-49-38-perturbations-speed-limit-2"
     # "${RESULTS_BASE}/gym/Walker2d/MaxVelocity-v3/2019-07-27T20-47-06-perturbations-speed-limit-2"
@@ -16,8 +16,8 @@ EXPERIMENT_PATHS=(
     # "${RESULTS_BASE}/gym/Hopper/v3/gym/Hopper/v3/2019-08-04T16-59-37-perturbations-claude-1/"
 
     # "${RESULTS_BASE}/gym/Humanoid/Stand-v3/gym/Humanoid/Stand-v3/2019-08-06T21-10-09-perturbations-1/"
-    # "${RESULTS_BASE}/gym/Humanoid/Stand-v3/gym/Humanoid/Stand-v3/2019-08-10T18-13-32-perturbations-2/" # 200
-    "${RESULTS_BASE}/gym/Humanoid/Stand-v3/gym/Humanoid/Stand-v3/2019-08-12T21-15-01-perturbations-ddpg-1/" # 100
+    "${RESULTS_BASE}/gym/Humanoid/Stand-v3/gym/Humanoid/Stand-v3/2019-08-10T18-13-32-perturbations-2/;200"
+    "${RESULTS_BASE}/gym/Humanoid/Stand-v3/gym/Humanoid/Stand-v3/2019-08-12T21-15-01-perturbations-ddpg-1/;100"
 
     # "${RESULTS_BASE}/gym/Hopper/v3/2019-09-03T12-24-29-perturbations-1/"
     # "${RESULTS_BASE}/gym/Walker2d/v3/2019-09-03T12-27-17-perturbations-1/"
@@ -28,21 +28,22 @@ EVALUATION_TASKS=(
     # "HeightField-v0"
     # "PerturbRandomAction-v0"
     # "PerturbNoisyAction-v0"
-    "PerturbBody-v0"
-    # "Wind-v0"
+    # "PerturbBody-v0"
+    "Wind-v0"
 )
 
+for EXPERIMENT_PATH_AND_CHECKPOINT_ID in ${EXPERIMENT_PATHS_AND_CHECKPOINT_IDS[@]}; do
+    IFS=";"; set -- ${EXPERIMENT_PATH_AND_CHECKPOINT_ID};
+    experiment_path="${1}"
+    checkpoint_id="${2}"
 
-for EXPERIMENT_PATH in ${EXPERIMENT_PATHS[@]}; do
-    for EVALUATION_TASK in ${EVALUATION_TASKS[@]}; do
-
-        echo "EXPERIMENT_PATH: ${EXPERIMENT_PATH}; EVALUATION_TASK: ${EVALUATION_TASK}"
+    for evaluation_task in ${EVALUATION_TASKS[@]}; do
 
         python -m examples.development.simulate_environments \
-               "${EXPERIMENT_PATH}" \
+               "${experiment_path}" \
                --num-rollouts=25 \
-               --evaluation-task="${EVALUATION_TASK}" \
-               --desired-checkpoint=100
+               --evaluation-task="${evaluation_task}" \
+               --desired-checkpoint="${checkpoint_id}"
 
     done
 done
