eeb30d11edb5fcd11efc94f5a82b46fd7ce4a301
Kristian Hartikainen <hartikainen@berkeley.edu>
2019-11-24 07:34:27 -0800

    Allow {Wind,PerturbBody}Wrapper to be used with 2d environments

--- softlearning/environments/gym/wrappers/perturb_body.py
+++ softlearning/environments/gym/wrappers/perturb_body.py
@@ -1,14 +1,37 @@
 import gym
 import numpy as np
 
+from gym.envs.mujoco import hopper_v3
+from gym.envs.mujoco import walker2d_v3
+from gym.envs.mujoco import ant_v3
+from gym.envs.mujoco import humanoid_v3
+
+
 from softlearning.utils.random import spherical as random_spherical
 
 
 __all__ = ['PerturbBodyWrapper']
 
 
+def num_dimensions(environment):
+    two_dimensional_envs = (
+        hopper_v3.HopperEnv,
+        walker2d_v3.Walker2dEnv,
+    )
+    three_dimensional_envs = (
+        ant_v3.AntEnv,
+        humanoid_v3.HumanoidEnv,
+    )
+    if isinstance(environment, two_dimensional_envs):
+        return 2
+    elif isinstance(environment, three_dimensional_envs):
+        return 3
+
+    raise NotImplementedError(type(environment))
+
+
 class PerturbBodyWrapper(gym.Wrapper):
-    """Rescale the action space of the environment."""
+    """Perturb the agent by applying a force to its torso."""
     def __init__(self,
                  *args,
                  perturbation_strength=0.0,
@@ -29,10 +52,31 @@ class PerturbBodyWrapper(gym.Wrapper):
 
         if self._step_counter % self._perturbation_frequency == 0:
             torso_index = self.sim.model.body_name2id('torso')
-            perturbation_direction = random_spherical(ndim=3)
+            perturbation_size = num_dimensions(self.unwrapped)
+            perturbation_direction = random_spherical(ndim=perturbation_size)
+
             perturbation  = (
                 perturbation_direction * self._perturbation_strength)
-            self.sim.data.xfrc_applied[torso_index][0:3] = perturbation
+            self.sim.data.xfrc_applied[torso_index][
+                0:perturbation_size] = perturbation
+
+        if hasattr(self, 'viewer') and self.viewer:
+            self.viewer.vopt.flags[:] = 0
+            self.viewer.vopt.flags[11] = 1
+            self.viewer.vopt.flags[12] = 1
+            self.viewer.vopt.flags[13] = 1
+
+        result = super(PerturbBodyWrapper, self).step(*args, **kwargs)
+
+        should_stop_perturbation = (
+            (self._step_counter % self._perturbation_frequency)
+            == self._perturbation_length - 1)
+
+        if should_stop_perturbation:
+            self.sim.data.xfrc_applied[:] = 0.0
+
+        return result
+
 
         if hasattr(self, 'viewer') and self.viewer:
             self.viewer.vopt.flags[:] = 0
--- softlearning/environments/gym/wrappers/wind.py
+++ softlearning/environments/gym/wrappers/wind.py
@@ -4,12 +4,14 @@ import numpy as np
 
 from softlearning.utils.random import spherical as random_spherical
 
+from .perturb_body import num_dimensions
+
 
 __all__ = ['WindWrapper']
 
 
 class WindWrapper(gym.Wrapper):
-    """Rescale the action space of the environment."""
+    """Perturb the agent by applying wind to the environment."""
     def __init__(self, *args, wind_strength=0, **kwargs):
         self._wind_strength = wind_strength
         result = super(WindWrapper, self).__init__(*args, **kwargs)
@@ -24,13 +26,14 @@ class WindWrapper(gym.Wrapper):
         self._step_counter += 1
 
         if self._step_counter % 150 == 0:
-            wind_direction = random_spherical(ndim=2)
+            perturbation_size = num_dimensions(self.unwrapped) - 1
+            wind_direction = random_spherical(ndim=perturbation_size)
             wind_xy = wind_direction * self._wind_strength
-            self.sim.model.opt.wind[0:2] = wind_xy
+            self.sim.model.opt.wind[0:perturbation_size] = wind_xy
 
         result = super(WindWrapper, self).step(*args, **kwargs)
 
         if (self._step_counter % 200) == 0:
-            self.sim.model.opt.wind[0:2] = 0.0
+            self.sim.model.opt.wind[:] = 0.0
 
         return result
