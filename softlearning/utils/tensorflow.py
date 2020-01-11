4af9adf366e627b35441725aad9d5a54f5c9fab6
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2020-01-11 16:40:58 +0000

    Use tf.python.util.nest instead of nest

--- softlearning/utils/tensorflow.py
+++ softlearning/utils/tensorflow.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 
 
 if LooseVersion(tf.__version__) > LooseVersion("2.00"):
-    from tensorflow import nest
+    from tensorflow.python.util import nest
 else:
     from tensorflow.contrib.framework import nest
 
