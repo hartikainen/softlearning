d316c613fe5b048770d9439a8a036375dd2b4686
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2020-01-30 20:42:27 +0000

    Clean the polynomial features models

--- softlearning/utils/numpy.py
+++ softlearning/utils/numpy.py
@@ -1,6 +1,16 @@
 import numpy as np
 
 
+def custom_combinations(n_features, degree):
+    from itertools import (
+        chain,
+        combinations_with_replacement as c_w_r
+    )
+    start = 1
+    return list(chain.from_iterable(
+        c_w_r(range(n_features), i) for i in range(start, degree + 1)))
+
+
 def softmax(x):
     max_x = np.max(x)
     exp_x = np.exp(x - max_x)
