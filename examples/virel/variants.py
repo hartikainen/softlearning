160852ef57aa83bb0598eb0994840c2de5aec13c
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2020-02-03 20:59:19 +0000

    Revert "Add sample_actions_from parameter for virel"
    This reverts commit d266d55e3e8397e6bb6cfebe3d01c82d364a05d5.

--- examples/virel/variants.py
+++ examples/virel/variants.py
@@ -35,7 +35,6 @@ def get_variant_spec(args):
 
             'reward_scale': 1.0,
             'discount': 0.99,
-            'sample_actions_from': tune.grid_search(['random', 'pool']),
             'Q_update_type': tune.grid_search([
                 # 'MSBBE',
                 'MSBE',
