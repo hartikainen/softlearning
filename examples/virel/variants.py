9d823152de5455d96c5b161bfa5c86f639cda285
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2020-02-05 00:05:46 +0000

    Add variants for uncertainty temperature sweep

--- examples/virel/variants.py
+++ examples/virel/variants.py
@@ -22,12 +22,13 @@ def get_variant_spec(args):
             'policy_lr': 3e-4,
             'Q_lr': 3e-4,
             'tau': 5e-3,
-            'beta_scale': tune.grid_search([1e-4, 1e-3, 1e-2, 1e-1, 1.0]),
+            # 'beta_scale': tune.grid_search([1e-4, 1e-3, 1e-2, 1e-1, 1.0]),
+            'beta_scale': tune.grid_search([1.0]),
             'beta_update_type': tune.grid_search([
                 # None,
-                'MSBE',
+                # 'MSBE',
                 # 'MSBBE',
-                # 'uncertainty',
+                'uncertainty',
             ]),
             'target_update_interval': 1,
 
@@ -50,22 +51,21 @@ def get_variant_spec(args):
 
     variant_spec['uncertainty_estimator_params'] = {
         'type': 'FeedforwardEnsemble',
-        'kwargs': tune.sample_from(lambda spec: {
-            'ensemble_size': (spec.get('config', spec)
-                              ['Q_params']
-                              ['kwargs']
-                              ['ensemble_size']),
+        'kwargs': {
+            'ensemble_size': 8,
             'output_size': 10,
-            'hidden_layer_sizes': (spec.get('config', spec)
-                                   ['Q_params']
-                                   ['kwargs']
-                                   ['hidden_layer_sizes']),
+            'hidden_layer_sizes': (256, 256),
             # 'prior_kernel_initializer': tf.random_normal_initializer(stddev=0.3),
-            'prior_kernel_initializer': (spec.get('config', spec)
-                                         ['Q_params']
-                                         ['kwargs']
-                                         ['prior_kernel_initializer'])
-        }),
+            'prior_kernel_initializer': {
+                'class_name': 'random_normal_initializer',
+                'config': {
+                    'mean': 0.0,
+                    # 'stddev': tune.grid_search([1e-4, 1e-3, 1e-2, 1e-1, 1.0]),
+                    # 'stddev': tune.grid_search([1e-1, 0.25, 0.5, 0.75, 1.0]),
+                    'stddev': tune.grid_search([1e-1, 2e-1, 3e-1, 5e-1]),
+                },
+            },
+        },
     }
 
     return variant_spec
