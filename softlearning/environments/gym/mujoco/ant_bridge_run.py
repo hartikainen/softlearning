Fix Ant-BridgeRun-v0 {on,after}_bridge_entropy computation

softlearning/environments/gym/mujoco/ant_bridge_run.py
@@ -112,10 +112,6 @@ class AntBridgeRunEnv(AntEnv):
             path['observations']['observations']
             for path in paths
         ], axis=0)
-        all_actions = np.concatenate([
-            path['actions']
-            for path in paths
-        ], axis=0)
 
         where_on_bridge = np.flatnonzero(
             all_observations[:, 0] <= self.bridge_length)
@@ -123,18 +119,18 @@ class AntBridgeRunEnv(AntEnv):
             self.bridge_length < all_observations[:, 0])
 
         on_bridge_observations = all_observations[where_on_bridge]
-        on_bridge_actions = all_actions[where_on_bridge]
         after_bridge_observations = all_observations[where_after_bridge]
-        after_bridge_actions = all_actions[where_after_bridge]
 
         if 0 < where_on_bridge.size:
-            on_bridge_entropy = self.policy.log_pis_np(
+            on_bridge_actions = self.policy.actions_np([on_bridge_observations])
+            on_bridge_entropy = -1.0 * self.policy.log_pis_np(
                 [on_bridge_observations], on_bridge_actions)
         else:
             on_bridge_entropy = np.nan
 
         if 0 < where_after_bridge.size:
-            after_bridge_entropy = self.policy.log_pis_np(
+            after_bridge_actions = self.policy.actions_np([after_bridge_observations])
+            after_bridge_entropy = -1.0 * self.policy.log_pis_np(
                 [after_bridge_observations], after_bridge_actions)
         else:
             after_bridge_entropy = np.nan